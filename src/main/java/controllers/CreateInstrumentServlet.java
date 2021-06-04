cf4c39f598f428270cfefe0bfb2c9403ecedbec6
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-06-04 16:55:25 -0500

    feat:started createServlet

---
+++ src/main/java/controllers/CreateInstrumentServlet.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import dao.DaoFactory;
+import models.Instrument;
+import models.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/instruments/create")
+public class CreateAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        Instrument instrument = new Instrument(
+                user.getId(),
+                request.getParameter("name"),
+                request.getParameter("description"),
+                request.getParameter("price"),
+                request.getParameter("")
+        );
+        DaoFactory.getInstrumentsDao().insert(instrument);
+        response.sendRedirect("/ads");
+    }
+}
