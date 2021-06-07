a0214cca5dd42f2d8cd7a3ed0afc582893aafe0b
Irfa Sheikh <irfasheikh@gmail.com>
2021-06-07 12:02:47 -0500

    Completed Ad Servlet

---
+++ src/main/java/controllers/AdServlet.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import dao.DaoFactory;
+import dao.Instruments;
+import models.Instrument;
+import models.User;
+import util.Password;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+
+
+@WebServlet(name = "AdServlet", urlPatterns = "/ViewInstrument")
+public class AdServlet extends HttpServlet {
+
+    protected void doGet (HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+
+        Long id = Long.parseLong(request.getParameter("id"));
+
+
+        //inside Ads dao takes in id and return single ad, use that in here
+
+      Instrument instrumentInfo= DaoFactory.getInstrumentsDao().getInstrumentById(id);
+        //now send this to jsp
+
+        request.setAttribute("SingleAd" , instrumentInfo);
+
+        request.getRequestDispatcher("/WEB-INF/instruments/InstrumentPage.jsp").forward(request, response);
+
+
+        }
+
+    }
+
+
