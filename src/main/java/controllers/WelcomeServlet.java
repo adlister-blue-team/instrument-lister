3db858bf1ee18b66fff36cb56b029ee5cf39b527
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-09 19:54:08 -0500

    set back

--- src/main/java/controllers/WelcomeServlet.java
+++ src/main/java/controllers/WelcomeServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "WelcomeServlet", urlPatterns = "/")
+@WebServlet(name = "WelcomeServlet", urlPatterns = "/welcome")
 public class WelcomeServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.getRequestDispatcher("/WEB-INF/welcome.jsp").forward(request, response);
